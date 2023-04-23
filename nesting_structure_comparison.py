c00a7258dd26388053fddc76cb64ea45b3d9af0f
Juanma Caba <129981416+JuanmaCaba@users.noreply.github.com>
2023-04-23 15:10:03 +0200

    Create nesting_structure_comparison.py

---
+++ nesting_structure_comparison.py
@@ -0,0 +1,15 @@
+#INSTRUCTIONS--->Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+
+def same_structure_as(original,other):
+    if type(original) != type(other):
+        return False
+    if len(original) == len(other) == 0:
+        return True
+    if len(original) != len(other):
+        return False
+    for i in range(len(original)):
+        if bool(type(original[i]) == list) != bool(type(other[i]) == list):
+            return False
+        elif type(original[i]) == type(other[i]) == list:
+            return same_structure_as(original[i], other[i])
+    return True
